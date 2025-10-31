f7d7ca2b679ea4caa2a8406d7dbeb4e7f0bed7c1
Sansut05 <silaswadlarsen@gmail.com>
2025-10-31 10:52:32 +0100

    first commit

---
+++ Mini-portfolio/index.js
@@ -0,0 +1,34 @@
+var currentPage = '#page1'
+
+
+function setup(){
+    console. log('P5 setup kaldt ')
+ //hent all side som et array
+ var allPages = selectAll('.page')
+ //løb listen igemmen en for en 
+    allPages.map(
+        (page) => {
+            //lave et ny <a> element
+            var menuItem = createElement('a')
+            // sæt a tagger html til sidne titel
+            menuItem.html(page.attribute('title'))
+            //sæt everlis på a tagger
+            menuItem.mousePressed(
+                () => shiftPage('#' + page.attribute('id'))
+                
+            )
+            //sæt a tagger ind i sidebare
+            select('.sidebar').child(menuItem)
+
+     }
+    )
+}
+
+
+
+
+function shiftPage(newPage){
+    select(currentPage).removeClass('show')
+    select(newPage).addClass('show')
+    currentPage = newPage
+}
