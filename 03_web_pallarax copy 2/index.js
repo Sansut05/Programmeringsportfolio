f63c6435105bc0865712976a811da32e36f63d5e
Sansut05 <commandersilas@gmail.com>
2026-01-12 21:05:15 +0100

    filte

--- 03_web_pallarax copy 2/index.js
+++ 03_web_pallarax copy 2/index.js
@@ -1,4 +1,4 @@
-var currentPage = '#page3'
+var currentPage = '#page4'
 var capture 
 var otterSound, rainSound
 var recBtn, recorder, AudioFile 
@@ -6,6 +6,37 @@ var isRecording = false
 var speakInp, speakBtn
 var listeInput, listeHeader, listeButton, listeContainer
 var removeListe 
+const fugle = [
+  "solsort","musvit","blåmejse","skovspurv","gråspurv","bogfinke","grønirisk",
+  "stillits","dompap","gærdesmutte","rødhals","sjagger","ringdue","bydue",
+  "hættemåge","sildemåge","svartbag","stormmåge","gråkrage","råge","allike",
+  "skade","husskade","nøddekrige","hærfugl","isfugl","svalehale","landsvale",
+  "bysvale","digesvale","tornsanger","munk","gransanger","løvsanger",
+  "rørsanger","sivsanger","havesanger","gulspurv","rørspurv","snespurv",
+  "korttået lærke","sanglærke","toplærke","bomlærke","piber","engpiber",
+  "skovpiber","bjergpiber","hvid vipstjert","gul vipstjert","citronvipstjert",
+  "vintergærdesmutte","sortstrubet bynkefugl","stenskvæt","buskskvæt",
+  "sortstrubet bynkefugl","nattergal","blåhals","rødstjert","husrødstjert",
+  "broget fluesnapper","grå fluesnapper","lille fluesnapper",
+  "sortmejse","topmejse","sortstrubet mejse","fyrremejse","sumpmejse",
+  "skægmejse","halemejse","pirol","silkehale","tornirisk","bjergirisk",
+  "lille korsnæb","stor korsnæb","hvidvinget korsnæb","kernebider",
+  "spurvehøg","duehøg","musvåge","fjeldvåge","hvepsevåge","rørhøg",
+  "blå kærhøg","rød glente","sort glente","havørn","kongeørn",
+  "tårnfalk","lærkefalk","jagtfalk","vandrefalk","slørugle",
+  "natugle","skovhornugle","hornugle","kirkeugle","spurveugle",
+  "perleugle","hjejle","stor regnspove","lille regnspove","brushane",
+  "rødben","sortklire","grønbenet rørhøne","hvidklire","mudderklire",
+  "dobbeltbekkasin","enkeltbekkasin","tinksmed","klyde","præstekrave",
+  "stor præstekrave","hjejle","strandskade","tejst","alk","lomvie",
+  "søkonge","lunde","skarv","topskarv","silkehejre","fiskehejre",
+  "rørdrum","sort stork","hvid stork","trane","blishøne","vandrikse",
+  "rørhøne","knopsvane","sangsvane","pibesvane","gråand","krikand",
+  "skeand","spidsand","atlingand","hvinand","troldand","toppet skallesluger",
+  "lille skallesluger","stor skallesluger","ederfugl","havlit",
+  "sortand","fløjlsand","bjergand","kongeederfugl","rødhalset lom",
+  "sortstrubet lom","hvidnæbbet lom"
+]
 function preload(){
   
     
@@ -62,7 +93,7 @@ function setup(){
        // make a list
        var elemets = ["hest", "dog", "hamster", "php", "cangaroo", "fuck", "sebatian", "rat" ]
        //call the greneut 
-       createList(elemets, removeListe, 'rapeVictim',  rape)
+       createList(elemets, removeListe, 'elev',  removeListItem)
 
        listeButton.mousePressed(()=>{
         if(listeInput.value() ==''){
@@ -85,8 +116,27 @@ function setup(){
         })
 
     //SOUND 
-   
-    
+   //page4 -filste suft
+
+   var  bridContainer = select('#bridContainer')
+   var birdInp = select('#birdInp')
+   createList(fugle, bridContainer, 'bird')
+    birdInp.input(() =>{
+       // console.log(birdInp.value())
+      var filterBird = fugle.filter( f =>{
+        return f.includes( birdInp.value())
+        //er der ind i f (en eller anden fugl), det der er i input feltet????
+      })
+      // nu er det nye array filterBirds fyldt med fugle der indeholder bogstaver fra input felotet
+      if(filterBird.length > 0){
+        createList(filterBird, bridContainer, 'bird')
+      }else{
+        var feedback = createElement('h2', "bird nit")
+        bridContainer.html("")
+        bridContainer.child(feedback)
+      }
+      
+    })
     //Sæt menu op
     //Hent alle sider som et array
     var allPages = selectAll('.page')
@@ -116,13 +166,13 @@ function shiftPage(newPage){
 function createList(list, dest, className, action){
     //førs søre 
     dest.html('')
-    list.map(e =>{
+    list.map((e, index) =>{
         var div = createDiv(e)
-        div.addClass('className')
+        div.addClass(className)
 
         if(action){
               div.mousePressed(() => {
-            action()
+            action(div, index, list)
         })
         }
         dest.child(div)
@@ -132,7 +182,12 @@ function createList(list, dest, className, action){
 }
 
 
-function rape(who){
+function removeListItem(who, index, list){
     console.log('sebastians was call', who)
-    who.style('background-image', url("/assets"))
+    who.style('background-image', `url("./assets/Foo_Fighters.jpg")`)
+    setTimeout(()=>{
+         list.splice(index, 1)
+        createList(list, removeListe, 'elev', removeListItem)
+    },800)
+    
 }
